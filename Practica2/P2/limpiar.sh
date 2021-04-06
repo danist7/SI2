663276a874da70051b389f2a237f4ea524ad8677
danist7 <daniel.santo-tomas@estudiante.uam.es>
2021-04-06 18:10:20 +0200

    dia productivo

---
+++ Practica2/P2/limpiar.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+for i in P1-base P1-ws P1-ejb-cliente-remoto; do
+cd $i
+ant replegar; ant delete-pool-local
+cd -
+done
+cd  P1-ejb-servidor-remoto
+ant replegar-todo
+cd -
+cd P1-base
+ant delete-db
+cd -
+for i in P1-base P1-ejb-servidor-remoto P1-ejb-cliente-remoto P1-ws; do
+cd $i
+ant limpiar-todo
+cd -
+done
+
+cd P1-base
+ant todo
+cd -
+cd P1-ejb-servidor-remoto
+ant p2
+cd -
+cd P1-ejb-cliente-remoto
+ant casitodo
+cd -
+cd P1-ws
+ant p2
+cd
